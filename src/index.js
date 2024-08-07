2a3d89c37b9a574b9892f3f71cca75c214d4ddc5
igor-rizak <igor.rizak@ukr.net>
2024-08-07 14:13:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <BrowserRouter basename="https://igor-rizak.github.io/React_FILMS/">
+     <BrowserRouter basename="/React_FILMS/">
       <App />
       </BrowserRouter>
   </React.StrictMode>
